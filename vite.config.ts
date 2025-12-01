0644dbcfe6729d23c540022ac924d0a1a533be27
Erkam Kavak <erkam.kavak@boun.edu.tr>
2025-12-01 22:29:45 +0300

    [update] config updates(tsconfig, vite config)

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [
+    svelte(),
+  ],
+  base: './',
+  build: {
+    assetsDir: 'assets'
+  }
 })
